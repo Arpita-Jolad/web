6ef0a124be3cee41481b96540b088d1fe8dafab3
Arpita-Jolad <95854008+Arpita-Jolad@users.noreply.github.com>
2023-02-07 12:27:14 +0530

    first commit

---
+++ programming/hello_world.c
@@ -0,0 +1,16 @@
+def palindrome1(n);
+#include<stdio.h>
+int main()
+{
+  int n,sum,temp,r,sum2;
+  temp=n;
+  while(n>0){
+      r=n%10
+      sum=(sum*10)+r;
+      n=n/10;
+  }
+  return sum;
+  if(sum==n){
+      printf("")
+
+}
